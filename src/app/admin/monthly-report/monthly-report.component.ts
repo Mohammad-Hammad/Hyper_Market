bdf00af59a8a2ed018c08f61321a28fe915f9259
mohammad hammad <mohammad212hammad@gmail.com>
2022-04-10 13:06:00 +0300

    UpdatePDFReportsWithIconsUpdate

--- src/app/admin/monthly-report/monthly-report.component.ts
+++ src/app/admin/monthly-report/monthly-report.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UserregisService } from 'src/app/service/userregis.service';
 import * as XLSX from 'xlsx';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-monthly-report',
   templateUrl: './monthly-report.component.html',
@@ -10,7 +12,7 @@ export class MonthlyReportComponent implements OnInit {
   title = 'angular-app';
   fileName= 'ExcelSheet.xlsx';
   constructor(public userRegisService:UserregisService) { }
-
+  @ViewChild('htmlData') htmlData!: ElementRef;
   ngOnInit(): void {
     this.userRegisService.getAllMonthlyReport();
     this.userRegisService.getMonthlySum();
@@ -29,5 +31,16 @@ export class MonthlyReportComponent implements OnInit {
     /* save to file */  
     XLSX.writeFile(wb, this.fileName);
  
-  }
+  }public openPDF(): void {
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('angular-demo.pdf');
+    });}
+  
 }
